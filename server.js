a5d3e6aee3ab8ba65462a7492c044d3fa6f77de7
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-07-24 12:31:59 -0700

    inital file-setup with starter code, did not have wifi so this is a temporary push, there are bugs in the code

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const db = require('./db/connection');
+// const apiRoutes = require('./routes/apiRoutes');
+
+//initialize console.table npm package
+//add .DS_Store to .gitignore
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// use ApiRoutes
+// app.use('/api', apiRoutes);
+
+// Default response for any other requests (Not Found)
+app.use((req, res) => {
+    res.status(400).end();
+});
+
+//Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Connected to the employees database');
+    app.listen(PORT, () => {
+        console.log(`Server running on port http://localhost:${PORT}`);
+    });
+});
