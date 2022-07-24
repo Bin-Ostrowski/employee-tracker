17385afd68d6e9b5ee92c5246dd95b08677442c9
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-07-24 15:49:09 -0700

    add get and post calls for employees, roles and departments. PUT call not working yet

--- server.js
+++ server.js
@@ -21,13 +21,9 @@ app.get('/', (req, res) => {
     });
   });
 
-// Default response for any other requests (Not Found)
-app.use((req, res) => {
-    res.status(400).end();
-});
-
-
-//test  // Connect to database
+  
+  
+  //test  // Connect to database
 const db = mysql.createConnection(
     {
         host: 'localhost',
@@ -40,11 +36,121 @@ const db = mysql.createConnection(
     console.log('Connected to the employees database.')
   );
 
-  db.query(`SELECT * FROM departments`, (err, rows) => {
-      console.log(rows);
-    });
+  //Get all departments
 
+app.get('/api/departments', (req, res) => {
+    const sql = `SELECT * FROM departments`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+  //GET all roles (link department_name from departments table)
+  app.get('/api/roles', (req, res) => {
+          const sql = `SELECT * FROM roles`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+});
+
+//GET all employees (link from roles(title, salaries) department, manager_id 
+app.get('/api/employees', (req, res) => {
+    const sql = `SELECT * FROM employees`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+});
+
+//POST new Department
+app.post('/api/departments', ({ body }, res) => {
+    const sql = `INSERT INTO departments (id, department_name)
+    VALUES (?,?)`;
+    const params = [9, "new department"]; //this will be from inquire input
     
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+
+//POST new employee
+app.post('/api/employees', ({ body }, res) => {
+    const sql = `INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+    VALUES (?,?,?,?,?)`;
+    const params = [body.id, body.first_name, body.last_name, body.role_id, body.manager_id];; //this will be from inquire input
+    
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+
+//UPDATE employee role // NEEDS FIXING!!!
+app.put('/api/employees', (req, res) => {
+    const sql = `UPDATE employees SET role_id = ? WHERE id = ?`;
+    const params = [req.body.role_id, req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if(err) {
+            res.status(400).json({ error: err.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Employee not found'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
+
+
+// Default response for any other requests (Not Found)
+app.use((req, res) => {
+    res.status(400).end();
+});
+
   app.listen(PORT, () => {
       console.log(`Server running on port http://localhost:${PORT}`);
     });
